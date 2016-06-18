c286c513a2b1151946ca79feb6d934ea65b98dd2
Thomas McGrew <tjmcgrew@gmail.com>
2016-06-18 19:52:11 -0400

    Added the ability to play multiple files

--- player.cpp
+++ player.cpp
@@ -82,113 +82,120 @@ static void start_track( int track, const char* path )
 int main( int argc, char** argv )
 {
 	init();
+  int fileidx = argc;
+  bool running = true;
 	
-	// Load file
-	const char* path = (argc > 1 ? argv [argc - 1] : "test.nsf");
-	handle_error( player->load_file( path ) );
-	start_track( 1, path );
-	
-	// Main loop
-	int track = 1;
-	double tempo = 1.0;
-	bool running = true;
-	double stereo_depth = 0.0;
-	bool accurate = false;
-	int muting_mask = 0;
-	while ( running )
-	{
-		SDL_Delay( 1000 / 100 );
-		
-		// Update scope
-		scope->draw( scope_buf, scope_width, 2 );
-		
-		// Automatically go to next track when current one ends
-		if ( player->track_ended() )
-		{
-			if ( track < player->track_count() )
-				start_track( ++track, path );
-			else
-				player->pause( paused = true );
-		}
-		
-		// Handle keyboard input
-		SDL_Event e;
-		while ( SDL_PollEvent( &e ) )
-		{
-			switch ( e.type )
-			{
-			case SDL_QUIT:
-				running = false;
-				break;
-			
-			case SDL_KEYDOWN:
-				int key = e.key.keysym.sym;
-				switch ( key )
-				{
-				case SDLK_q:
-				case SDLK_ESCAPE: // quit
-					running = false;
-					break;
-				
-				case SDLK_LEFT: // prev track
-					if ( !paused && !--track )
-						track = 1;
-					start_track( track, path );
-					break;
-				
-				case SDLK_RIGHT: // next track
-					if ( track < player->track_count() )
-						start_track( ++track, path );
-					break;
-				
-				case SDLK_MINUS: // reduce tempo
-					tempo -= 0.1;
-					if ( tempo < 0.1 )
-						tempo = 0.1;
-					player->set_tempo( tempo );
-					break;
-				
-				case SDLK_EQUALS: // increase tempo
-					tempo += 0.1;
-					if ( tempo > 2.0 )
-						tempo = 2.0;
-					player->set_tempo( tempo );
-					break;
-				
-				case SDLK_SPACE: // toggle pause
-					paused = !paused;
-					player->pause( paused );
-					break;
-				
-				case SDLK_a: // toggle accurate emulation
-					accurate = !accurate;
-					player->enable_accuracy( accurate );
-					break;
-				
-				case SDLK_e: // toggle echo
-					stereo_depth += 0.2;
-					if ( stereo_depth > 0.5 )
-						stereo_depth = 0;
-					player->set_stereo_depth( stereo_depth );
-					break;
-				
-				case SDLK_0: // reset tempo and muting
-					tempo = 1.0;
-					muting_mask = 0;
-					player->set_tempo( tempo );
-					player->mute_voices( muting_mask );
-					break;
-				
-				default:
-					if ( SDLK_1 <= key && key <= SDLK_9 ) // toggle muting
-					{
-						muting_mask ^= 1 << (key - SDLK_1);
-						player->mute_voices( muting_mask );
-					}
-				}
-			}
-		}
-	}
+  while ( running ) {
+    // Load file
+    --fileidx;
+    if (fileidx < 1)
+      exit(0);
+    const char* path = argv [fileidx];
+    handle_error( player->load_file( path ) );
+    start_track( 1, path );
+    
+    // Main loop
+    int track = 1;
+    double tempo = 1.0;
+    double stereo_depth = 0.0;
+    bool accurate = true;;
+    int muting_mask = 0;
+    while ( running )
+    {
+      SDL_Delay( 1000 / 100 );
+      
+      // Update scope
+      scope->draw( scope_buf, scope_width, 2 );
+      
+      // Automatically go to next track when current one ends
+      if ( player->track_ended() )
+      {
+        if ( track < player->track_count() )
+          start_track( ++track, path );
+        else
+          break;
+//          player->pause( paused = true );
+      }
+      
+      // Handle keyboard input
+      SDL_Event e;
+      while ( SDL_PollEvent( &e ) )
+      {
+        switch ( e.type )
+        {
+        case SDL_QUIT:
+          running = false;
+          break;
+        
+        case SDL_KEYDOWN:
+          int key = e.key.keysym.sym;
+          switch ( key )
+          {
+          case SDLK_q:
+          case SDLK_ESCAPE: // quit
+            running = false;
+            break;
+          
+          case SDLK_LEFT: // prev track
+            if ( !paused && !--track )
+              track = 1;
+            start_track( track, path );
+            break;
+          
+          case SDLK_RIGHT: // next track
+            if ( track < player->track_count() )
+              start_track( ++track, path );
+            break;
+          
+          case SDLK_MINUS: // reduce tempo
+            tempo -= 0.1;
+            if ( tempo < 0.1 )
+              tempo = 0.1;
+            player->set_tempo( tempo );
+            break;
+          
+          case SDLK_EQUALS: // increase tempo
+            tempo += 0.1;
+            if ( tempo > 2.0 )
+              tempo = 2.0;
+            player->set_tempo( tempo );
+            break;
+          
+          case SDLK_SPACE: // toggle pause
+            paused = !paused;
+            player->pause( paused );
+            break;
+          
+          case SDLK_a: // toggle accurate emulation
+            accurate = !accurate;
+            player->enable_accuracy( accurate );
+            break;
+          
+          case SDLK_e: // toggle echo
+            stereo_depth += 0.2;
+            if ( stereo_depth > 0.5 )
+              stereo_depth = 0;
+            player->set_stereo_depth( stereo_depth );
+            break;
+          
+          case SDLK_0: // reset tempo and muting
+            tempo = 1.0;
+            muting_mask = 0;
+            player->set_tempo( tempo );
+            player->mute_voices( muting_mask );
+            break;
+          
+          default:
+            if ( SDLK_1 <= key && key <= SDLK_9 ) // toggle muting
+            {
+              muting_mask ^= 1 << (key - SDLK_1);
+              player->mute_voices( muting_mask );
+            }
+          }
+        }
+      }
+    }
+  }
 	
 	// Cleanup
 	delete player;
